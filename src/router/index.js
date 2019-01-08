cf274fc582ae3bb25ea8c912257be4d7108e0cbf
jiahua123 <1372888048@qq.com>
2019-01-08 18:57:48 +0800

    添加searchlist目录中的路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import Search from '@/components/Search';
+		import Womenc from '@/components/Searchlist/Womenc';
+		import Menc from '@/components/Searchlist/Menc';
+		import Childrenc from '@/components/Searchlist/Childrenc';
+			
 import '@/App';
 
 Vue.use(Router);
@@ -13,11 +17,33 @@ routes: [
       name: 'Home',
       component: Home
     },
+    
     {
-		name:'Search',
+//		name:'Search',
 		path:'/search',
 		component:Search,
-		props:true
+		 children:[
+        {
+          path:'',//默认子路由
+          redirect:{name:'Womenc'}
+        },
+        {
+          path:'womenc',
+          name:'Womenc',
+          component:Womenc
+        },
+        {
+          path:'menc',
+          name:'Menc',
+          component:Menc
+        },
+      	  {
+          path:'childrenc',
+          name:'Childrenc',
+          component:Childrenc
+        }
+      ]
+		
 	}
 ]
 });
