8f9a59e212171ace46b7e23c8b2bc4ccf8f686cc
jiahua123 <18825073715@163.com>
2019-01-08 22:16:35 +0800

    创建cart路由

---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="cart">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	
+	
+</style>
