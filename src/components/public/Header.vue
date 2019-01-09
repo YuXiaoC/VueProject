bf85a9b4f9365379c3a7facb6bb2d817dea1a8e2
yuyu <1912850912@qq.com>
2019-01-09 11:04:33 +0800

    修改点击标题切回主页2019-1-9 11:04

--- src/components/public/Header.vue
+++ src/components/public/Header.vue
@@ -1,47 +1,55 @@
 <template>
   <div class="nav">
-     <!-- <slot></slot>  --> 
-    <mt-header title="FARFETCH">
-      <router-link to="/" slot="left" class="more">
-        <myicons name="three-bars" class="three-bars"></myicons>
-      </router-link>
-      <router-link to="/search" slot="left" class="search">
-        <myicons name="search"></myicons>
-      </router-link>
-      <!-- <mt-button icon="more" slot="right" class=""></mt-button> -->
-      <router-link to="/cart" slot="right">
-        <myicons name="star" class="star"></myicons>
-        <myicons name="heart" class="heart"></myicons>
-      </router-link>
-    </mt-header>
+    <!-- <slot></slot>  -->
+    <router-link  to="/">
+      <mt-header title="FARFETCH">
+        <router-link to="/slider" slot="left" class="more">
+          <myicons name="three-bars" class="three-bars"></myicons>
+        </router-link>
+        <router-link to="/search" slot="left" class="search">
+          <myicons name="search"></myicons>
+        </router-link>
+        <!-- <mt-button icon="more" slot="right" class=""></mt-button> -->
+        <router-link to="/cart" slot="right">
+          <myicons name="star" class="star"></myicons>
+          <myicons name="heart" class="heart"></myicons>
+        </router-link>
+      </mt-header>
+    </router-link>
   </div>
 </template>
 <script>
-import Vue from 'vue';
-import MintUI from 'mint-ui';//引入mint-ui插件
+import Vue from "vue";
+import MintUI from "mint-ui"; //引入mint-ui插件
 Vue.use(MintUI);
-import 'mint-ui/lib/style.css';//引入mint-ui插件样式
-import myicons from '@/plugins/icons.js';//引入我们自定义图标组件
+import "mint-ui/lib/style.css"; //引入mint-ui插件样式
+import myicons from "@/plugins/icons.js"; //引入我们自定义图标组件
 Vue.use(myicons);
 export default {
-    created(){
-        console.log(666);
-    }
+  created() {
+    console.log(666);
+  }
 };
 </script>
 <style>
-.nav .more,.search,.star{
+.nav .more,
+.search,
+.star {
   float: left;
   fill: azure;
 }
-.nav .search ,.star{
+.nav .search,
+.star {
   margin-left: 20px;
 }
-.nav .heart{
+.nav .heart {
   fill: azure;
 }
-.nav .mint-header-title{
+.nav .mint-header-title {
   font-weight: 600;
   font-size: 23px;
+}
+.nav .router-link-active {
+ text-decoration: none;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,77 +2,59 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import Search from '@/components/Search';
-		import Womenc from '@/components/Searchlist/Womenc';
-		import Menc from '@/components/Searchlist/Menc';
-		import Childrenc from '@/components/Searchlist/Childrenc';
+import Womenc from '@/components/Searchlist/Womenc';
+import Menc from '@/components/Searchlist/Menc';
+import Childrenc from '@/components/Searchlist/Childrenc';
 import Cart from '@/components/Cart';
-	
+import Slider from '@/components/slider';
+
 import '@/App';
 
 Vue.use(Router);
 
 export default new Router({
-routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Home',
       component: Home
     },
-    
+
     {
-//		name:'Search',
-		path:'/search',
-		component:Search,
-		 children:[
-        {
-          path:'',//默认子路由
-          redirect:{name:'Womenc'}
+      //		name:'Search',
+      path: '/search',
+      component: Search,
+      children: [{
+          path: '', //默认子路由
+          redirect: {
+            name: 'Womenc'
+          }
         },
         {
-          path:'womenc',
-          name:'Womenc',
-          component:Womenc
+          path: 'womenc',
+          name: 'Womenc',
+          component: Womenc
         },
         {
-          path:'menc',
-          name:'Menc',
-          component:Menc
+          path: 'menc',
+          name: 'Menc',
+          component: Menc
         },
-      	  {
-          path:'childrenc',
-          name:'Childrenc',
-          component:Childrenc
+        {
+          path: 'childrenc',
+          name: 'Childrenc',
+          component: Childrenc
         }
       ]
-		
-	},
-	{
-		name:'Cart',
-		path:'/cart',
-		component:Cart,
-	}
-]
+
+    },
+    {
+      name: 'Cart',
+      path: '/cart',
+      component: Cart,
+    },
+    {
+      path:'/slider',
+      component:Slider
+    }
+  ]
 });
-
-//const routes=[
-//	 {
-//    path: '/',
-//    name: 'Home',
-//    component: Home
-//  },
-//  {
-//		name:'Search',
-//		path:'/search',
-//		component:Search,
-//		props:true
-//	}
-//];
-//
-//
-//let router =new Router({
-//	 routes
-// });
-//
-//export default router;
-
-
