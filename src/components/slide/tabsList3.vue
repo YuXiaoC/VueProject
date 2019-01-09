8562a3bf94db8692a5bc5b277d7182dcda3af845
jiahua123 <18825073715@163.com>
2019-01-09 22:56:56 +0800

    Create tabsList3.vue

---
+++ src/components/slide/tabsList3.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="ListBox">
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <mt-cell title="儿童"/>
+        <mt-cell v-for="item in tabList" :title="item.title" :key="item.id">
+             <myicons name="chevron-right"></myicons>
+        </mt-cell>
+        <router-view></router-view>
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabList: [
+        {
+          id: 1,
+          title: "儿童1"
+        },
+        {
+          id: 2,
+          title: "儿童2"
+        },
+        {
+          id: 3,
+          title: "儿童3"
+        },
+        {
+          id: 4,
+          title: "儿童4"
+        },
+        {
+          id: 5,
+          title: "儿童5"
+        }
+      ],
+      selected:'1'
+    };
+  }
+};
+</script>
+
+<style>
+.mint-cell{
+  border-bottom:1px solid #ccc;
+}
+</style>
