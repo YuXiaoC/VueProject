13859364af3babefce6dfa8483699ff22df29b47
jiahua123 <18825073715@163.com>
2019-01-14 16:15:57 +0800

    完成

---
+++ src/components/Detailpages/detailtu.vue
@@ -0,0 +1,24 @@
+<template>
+		<div class="detailtu">
+			<mt-swipe :auto="0">
+				  <mt-swipe-item>
+				  	<img :src="shangping.images.cutOut"/>
+				  </mt-swipe-item>
+			</mt-swipe>
+		</div>		
+</template>
+
+<script>
+	 export default {
+	 	 props: [
+					 'shangping'
+					 ],
+	 	data(){
+	 			return {}
+	 		},
+		 }
+</script>
+
+<style lang="scss">
+	
+</style>
